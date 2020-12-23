3ac1f0ba4261c15f3f51d6acc900b5f7caf28e3b
unknown <jhonjlinaresb@gmail.com>
2020-12-23 21:36:44 +0100

    Changes in comments of index.js

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.get('/users/showAll', showUsers);
 app.get('/users/id/:userId', showUsersId);
 app.get('/users/:dni/tickets', userTickets);
 
-app.post('/users/register', registerUser); //Ya 
-app.post('/users/login', loginUser); //Ya
+app.post('/users/register', registerUser); 
+app.post('/users/login', loginUser); 
 app.post('/users/logout', logoutUser);
 app.post('/users/:dni/tickets', adduserTicket);
 
